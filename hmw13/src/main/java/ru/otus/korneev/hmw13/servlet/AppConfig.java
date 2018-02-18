37d617407cade93c650d83b8bf66ecd88028514c
KorneevVV <korneev.vladislav.v@gmail.com>
2018-02-18 12:32:01 +0300

    hmw13 fx

---
+++ hmw13/src/main/java/ru/otus/korneev/hmw13/servlet/AppConfig.java
@@ -0,0 +1,35 @@
+package ru.otus.korneev.hmw13.servlet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.otus.korneev.hmw10.dbService.DBService;
+import ru.otus.korneev.hmw11.Cache.CacheEngine;
+import ru.otus.korneev.hmw11.Cache.CacheEngineImpl;
+import ru.otus.korneev.hmw11.DBServiceWithCache.DBServiceImpWithCache;
+import ru.otus.korneev.hmw12.service.AccountService;
+import ru.otus.korneev.hmw12.service.AccountServiceImpl;
+
+import java.sql.SQLException;
+
+@Configuration
+public class AppConfig {
+
+    @Autowired
+    private CacheEngine<Long, Object> cacheEngine;
+
+    @Bean
+    public AccountService getAccountService() {
+        return new AccountServiceImpl();
+    }
+
+    @Bean
+    public CacheEngine getCacheEngine() {
+        return new CacheEngineImpl(2, 10000, 0, false);
+    }
+
+    @Bean
+    public DBService getDBService() throws SQLException {
+        return new DBServiceImpWithCache(cacheEngine);
+    }
+}
